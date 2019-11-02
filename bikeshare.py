566e40149645e6064a30add9523a6afd4e9f8cd3
Rahul Sheth <rahul.sheth90@gmail.com>
2019-11-02 13:07:29 +0530

    Added functions to main() post debugging

--- bikeshare.py
+++ bikeshare.py
@@ -234,10 +234,10 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
-        #time_stats(df)
-        #station_stats(df)
-        #trip_duration_stats(df)
-        #user_stats(df)
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
         print_data(df, 5)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
